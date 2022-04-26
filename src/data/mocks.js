83dd7f7ae11d8aca97124c5011f18676ead66329
alfazari <faa04807@gmail.com>
2022-04-26 18:12:10 +0700

    feat: try components with mock data

--- src/data/mocks.js
+++ src/data/mocks.js
@@ -1,4 +1,4 @@
-[
+const data = [
   { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
   { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
   { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
@@ -8,3 +8,5 @@
   { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
   { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
 ];
+
+export default data;
