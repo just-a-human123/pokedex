b3fa1e597586ee3ab464375433a85b244c43a684
alfazari <faa04807@gmail.com>
2022-04-27 14:10:13 +0700

    minor change style

--- src/components/pokestyle.js
+++ src/components/pokestyle.js
@@ -2,12 +2,12 @@ import { css } from "@emotion/css";
 
 export const PokeCardStyle = css`
     width: 300px;
-    border: 1px solid black;
-    border-radius: 5px;
+    border-radius: 50px;
     box-shadow: 7px 10px 12px -5px rgba(0,0,0,0.75);
     margin: 1rem;
     padding: 1rem;
     text-align: center;
+    background-color: lightblue;
 `;
 
 export const PokeDexStyle = css`
